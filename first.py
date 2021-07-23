a69e5f7f0eef0784de33392e7df3cbc9852c94c4
Alina <Alin4ikboronina112@gmail.com>
2021-07-23 14:31:05 +0300

    ohhh_2

--- first.py
+++ first.py
@@ -1,3 +1,3 @@
 print("sooo it's my first repository")
 print("i'm learning py and listening a playlist on youtube rn")
-print("i wanna create a project in a future")
+print("aaaaand___ i wanna create a project in a future")
